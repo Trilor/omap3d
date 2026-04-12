043eff42de30a3b4f1d97e17b4945de8b90d35ce
Trilor <takogurage.re@gmail.com>
2026-04-12 10:54:23 +0900

    Update thumbnails and UI styles

---
+++ .wrangler/tmp/pages-sUY9gB/functionsRoutes-0.2594896323513953.mjs
@@ -0,0 +1,11 @@
+import { onRequest as __qchizu___path___js_onRequest } from "C:\\Users\\kurag\\Documents\\GitHub\\TeleDrop\\functions\\qchizu\\[[path]].js"
+
+export const routes = [
+    {
+      routePath: "/qchizu/:path*",
+      mountPath: "/qchizu",
+      method: "",
+      middlewares: [],
+      modules: [__qchizu___path___js_onRequest],
+    },
+  ]
